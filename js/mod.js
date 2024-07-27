0c06fbd59c2f268fd4d20caa7f8699db503ff0db
GuppyZed <aaron.havenar@outlook.com>
2024-07-26 21:10:21 -0400

    Update mod.js
    update name, author, and ID

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The HVAC Tree",
+	id: "HVACTree",
+	author: "GuppyZed",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
